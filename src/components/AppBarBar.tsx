50da2ebe0bbaf259942e9c555119d6e2efd297fc
Aleksandra-Romanic <aleksandra.romanic@productdock.com>
2024-09-12 20:49:20 +0200

    Updated search in AppBar

--- src/components/AppBarBar.tsx
+++ src/components/AppBarBar.tsx
@@ -1,5 +1,5 @@
 import { Search } from "@mui/icons-material";
-import { AppBar, Box, IconButton, OutlinedInput, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, IconButton, OutlinedInput, TextField, Toolbar, Typography } from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
 import PersonIcon from '@mui/icons-material/Person';
 import pd from '../assets/Vector.svg'
@@ -10,14 +10,20 @@ import user from '../assets/UserPhoto.svg'
 export const AppBarBar = () => {
     return (
         <Box sx={{ flexGrow: 1 }}>
-          <AppBar position="static" style={{ background: '#FFFFFF' }}>
+          <AppBar position="static" sx={{ background:"white", height:"64px"}}>
             <Toolbar>
               <Typography style={{ color: 'black'}} > <LogoBar/></Typography>
-              <OutlinedInput
+              <TextField id="outlined-basic"  variant="outlined" sx={{width:"370px", height:"50px", gap:"4px",
+               outline: "none", background: "#F7F7F9", borderRadius: "20px"} }
+              
+              placeholder="Search for title or author"
               style={{margin: 'auto'}}
-              endAdornment = {<IconButton><SearchIcon/></IconButton>}
+              slotProps={{input:{startAdornment:(
+                <IconButton><SearchIcon/></IconButton>
+    )}}}
               />
-              <IconButton edge = 'end'><img src={user} alt='logo' className="pd-logo logo"   /></IconButton>
+            
+              <IconButton edge = 'end'> <img src={user} alt='logo' className="pd-logo logo"   /></IconButton>
             </Toolbar>
           </AppBar>
         </Box>
